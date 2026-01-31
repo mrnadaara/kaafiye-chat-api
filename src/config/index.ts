206308c0b80aa86d8229e7845a17926f11e75bd5
Sharmaarke Ahmed <s.mustaf.m@gmail.com>
2026-01-31 14:38:32 +0000

    setup project directory

---
+++ src/config/index.ts
@@ -0,0 +1,5 @@
+export const mongoDbConnString = process.env.MONGODB_CONNECTION_STRING
+export const serverPort = process.env.PORT
+export const jwtSecret = process.env.JWT_SECRET
+export const jwtAudience = process.env.JWT_AUDIENCE
+export const jwtIssuer = process.env.JWT_ISSUER
